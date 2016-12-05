5bb6c2e98a08f0a0cf7360e6a8e445617d5600bb
nalbam-ya <jyyu@yanolja.in>
2016-12-05 16:26:08 +0900

    first commit

---
+++ app/src/main/java/com/nalbam/smsreceiver/MainActivity.java
@@ -0,0 +1,29 @@
+package com.nalbam.smsreceiver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button smsBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smsBtn = (Button) findViewById(R.id.btn_sms);
+
+        smsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SMSActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
