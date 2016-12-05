cd63946fd8f49e868d075236808cfacd43e1f4d0
nalbam-ya <jyyu@yanolja.in>
2016-12-05 18:02:10 +0900

    sms_receive

--- app/src/main/java/com/nalbam/smsreceiver/MainActivity.java
+++ app/src/main/java/com/nalbam/smsreceiver/MainActivity.java
@@ -1,29 +1,80 @@
 package com.nalbam.smsreceiver;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
-
-    private Button smsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        smsBtn = (Button) findViewById(R.id.btn_sms);
-
-        smsBtn.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SMSActivity.class);
-                startActivity(intent);
+                MainActivityPermissionsDispatcher.needSMSWithCheck(MainActivity.this);
             }
         });
+    }
+
+    @NeedsPermission(Manifest.permission.RECEIVE_SMS)
+    void needSMS() {
+        Toast.makeText(this, "권환 허용", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @OnShowRationale(Manifest.permission.RECEIVE_SMS)
+    void showRationaleForSMS(PermissionRequest request) {
+        showRationaleDialog("SMS 수신 권한 요청", request);
+    }
+
+    @OnPermissionDenied(Manifest.permission.RECEIVE_SMS)
+    void onSMSDenied() {
+        Toast.makeText(this, "거부", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.RECEIVE_SMS)
+    void onSMSNeverAskAgain() {
+        Toast.makeText(this, "다시 묻지 않음", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showRationaleDialog(String message, final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton("허용 요청", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("거부", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage(message)
+                .show();
     }
 
 }
--- app/src/main/java/com/nalbam/smsreceiver/SMSBroadcast.java
+++ app/src/main/java/com/nalbam/smsreceiver/SMSBroadcast.java
@@ -5,14 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * Created by nalbam on 2016. 12. 5..
- */
 public class SMSBroadcast extends BroadcastReceiver {
 
     @Override
@@ -21,27 +19,39 @@ public class SMSBroadcast extends BroadcastReceiver {
 
         if ("android.provider.Telephony.SMS_RECEIVED".equals(action)) {
             Bundle bundle = intent.getExtras();
+            if (bundle == null) {
+                return;
+            }
+
             Object messages[] = (Object[]) bundle.get("pdus");
+            if (messages == null) {
+                return;
+            }
+
             SmsMessage smsMessage[] = new SmsMessage[messages.length];
 
             for (int i = 0; i < messages.length; i++) {
                 smsMessage[i] = SmsMessage.createFromPdu((byte[]) messages[i]);
             }
 
+            this.abortBroadcast();
+
             Date curDate = new Date(smsMessage[0].getTimestampMillis());
             SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy년 MM월 HH시 mm분 ss초 ", Locale.KOREA);
 
             String originDate = mDateFormat.format(curDate);
             String origNumber = smsMessage[0].getOriginatingAddress();
-            String Message = smsMessage[0].getMessageBody().toString();
+            String Message = smsMessage[0].getMessageBody();
 
-            Intent showSMSIntent = new Intent(mContext, ShowSMSActivity.class);
-            showSMSIntent.putExtra("originNum", origNumber);
-            showSMSIntent.putExtra("smsDate", originDate);
-            showSMSIntent.putExtra("originText", Message);
-            showSMSIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            Toast.makeText(mContext, Message, Toast.LENGTH_LONG).show();
 
-            mContext.startActivity(showSMSIntent);
+//            Intent showSMSIntent = new Intent(mContext, ShowSMSActivity.class);
+//            showSMSIntent.putExtra("originNum", origNumber);
+//            showSMSIntent.putExtra("smsDate", originDate);
+//            showSMSIntent.putExtra("originText", Message);
+//            showSMSIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//
+//            mContext.startActivity(showSMSIntent);
         }
     }
 
